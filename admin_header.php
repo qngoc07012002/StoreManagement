f60252ddf6993f2ef0dd2dd63740e6520ab8a1c0
qngoc07012002 <116235732+qngoc07012002@users.noreply.github.com>
2023-06-23 11:02:18 +0700

    Upload Header

--- admin_header.php
+++ admin_header.php
@@ -16,7 +16,7 @@ if(isset($message)){
 <header class="header"> 
    <div class="flex">
 
-   <a href="" class="logo">Store<span>Management</span></a>
+   <a href="" class="logo">ATN<span>Management</span></a>
     
 
       <nav class="navbar">
